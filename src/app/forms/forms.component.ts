9913b8254133840997063316e67062ccc7e2d2e7
Luka Samkharadze <samkharadzeluke@gmail.com>
2021-01-17 18:33:39 +0400

    feat(forms): switch between pages

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+type ComponentType = "register" | "list";
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent{
-  constructor() { }
+export class FormsComponent {
+  current: ComponentType = "register";
 }
