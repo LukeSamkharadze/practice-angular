4f2e968699da2c1080ba61a6cd873adbeaaf9df3
Luka Samkharadze <samkharadzeluke@gmail.com>
2021-01-20 02:15:33 +0400

    feat(currency): add first working inputs prototype

--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import * as CurrenciesResponse from "./models/rates";
 
 @Component({
   selector: 'app-currency-converter',
@@ -6,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency-converter.component.scss']
 })
 export class CurrencyConverterComponent implements OnInit {
+  currencies: string[] = [];
+  observable: Observable<Object> | undefined;
 
-  constructor() { }
+  formGroup: FormGroup = new FormGroup({
+    default: new FormControl("0"),
+    optional: new FormArray([])
+  })
+
+  constructor(private httpClient: HttpClient) {
+    this.observable = httpClient.get("https://api.exchangeratesapi.io/latest");
+    this.observable.subscribe(o => this.handleRates(o));
+  }
+
+  handleRates(response: any) {
+    for (let currencyName in response.rates)
+      this.currencies.push(currencyName);
+  }
+
+  addNewCurrency() {
+    (this.formGroup.get("optional") as FormArray).push(new FormControl("0"))
+  }
+
+  getAddedControls(): AbstractControl[] {
+    return (this.formGroup.get("optional") as FormArray).controls;
+  }
 
   ngOnInit(): void { }
 }
--- src/app/currency-converter/currency-converter.module.ts
+++ src/app/currency-converter/currency-converter.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurrencyConverterComponent } from './currency-converter.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [CurrencyConverterComponent],
+  declarations: [
+    CurrencyConverterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   exports: [
     CurrencyConverterComponent
