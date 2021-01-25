fb6b62e5555adfae6d116d766a914e7c77afb44e
Luka Samkharadze <samkharadzeluke@gmail.com>
2021-01-25 16:20:41 +0400

    feat(employees): add pagination on employees

--- src/app/employees/employees-list/employees-list.component.ts
+++ src/app/employees/employees-list/employees-list.component.ts
@@ -7,11 +7,35 @@ import { Employee } from '../models/employee';
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.scss']
 })
-export class EmployeesListComponent  {
-
+export class EmployeesListComponent {
   employees: Employee[] = [];
+  currentPageEmployees: Employee[] = [];
 
-  constructor(private employeesService: EmployeesService ) { 
-    this.employeesService.getEmployees().subscribe(o => this.employees = o)
+  maxEmployeesOnPage = 2;
+  currentPage = 0;
+
+  constructor(private employeesService: EmployeesService) {
+    this.employeesService.getEmployees().subscribe(o => this.handleEmployees(o));
+  }
+
+  handleEmployees(employees: Employee[]) {
+    this.employees = employees;
+    this.updateCurrentPageEmployees();
+  }
+
+  updateCurrentPageEmployees() {
+    this.currentPageEmployees = this.employees.slice(this.currentPage * this.maxEmployeesOnPage, (this.currentPage + 1) * this.maxEmployeesOnPage)
+  }
+
+  leftClicked() {
+    if (this.currentPage) {
+      this.currentPage--;
+      this.updateCurrentPageEmployees();
+    }
+  }
+
+  rightClicked() {
+    this.currentPage++;
+    this.updateCurrentPageEmployees();
   }
 }
